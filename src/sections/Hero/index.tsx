99d0e03055328bda2efb19d627e82ca96fea588e
KaiWhen <wangkw523@gmail.com>
2025-07-30 15:58:44 +0100

    try moving left leaf up

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -74,19 +74,17 @@ export default function Hero() {
           delay: 1,
         });
 
-        if (!isMobile) {
-          gsap
-            .timeline({
-              scrollTrigger: {
-                trigger: "#hero",
-                start: "top top",
-                end: "bottom top",
-                scrub: true,
-              },
-            })
-            .to(".right-leaf", { y: 200 }, 0)
-            .to(".left-leaf", { y: -200 }, 0);
-        }
+        gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: "#hero",
+              start: "top top",
+              end: "bottom top",
+              scrub: true,
+            },
+          })
+          .to(".right-leaf", { y: 200 }, 0)
+          .to(".left-leaf", { y: -200 }, 0);
 
         // Only create video timeline when video is loaded
         if (videoLoaded && videoRef.current && !isMobile) {
